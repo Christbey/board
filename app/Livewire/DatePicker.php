eb83f2926c3da2e3961d237ecb3d6a9ac7a05530
Bey <Josh@picksports.app>
2024-06-06 19:56:11 -0500

    date filter

--- app/Livewire/DatePicker.php
+++ app/Livewire/DatePicker.php
@@ -2,34 +2,33 @@
 
 namespace App\Livewire;
 
-
 use Livewire\Component;
 use Carbon\Carbon;
 
 class DatePicker extends Component
 {
-public $sport;
-public $date;
+    public $sport;
+    public $date;
 
-public function mount($sport)
-{
-$this->sport = $sport;
-$this->date = Carbon::today()->format('Y-m-d');
-}
+    public function mount($sport)
+    {
+        $this->sport = $sport;
+        $this->date = request('date', Carbon::today()->format('Y-m-d'));
+    }
 
-public function updatedDate()
-{
-$this->filter();
-}
+    public function updatedDate()
+    {
+        $this->filter();
+    }
 
-public function filter()
-{
-$routeName = strtolower($this->sport) . '.odds';
-return redirect()->route($routeName, ['date' => $this->date]);
-}
+    public function filter()
+    {
+        $routeName = strtolower($this->sport) . '.odds';
+        return redirect()->route($routeName, ['date' => $this->date]);
+    }
 
-public function render()
-{
-return view('livewire.date-picker');
-}
+    public function render()
+    {
+        return view('livewire.date-picker');
+    }
 }
