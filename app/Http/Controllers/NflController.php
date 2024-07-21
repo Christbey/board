ce20243b9f92a4fc0b824ddc040d8654ee18bb09
Bey <Josh@picksports.app>
2024-07-20 21:19:56 -0500

    team view part 2.

--- app/Http/Controllers/NflController.php
+++ app/Http/Controllers/NflController.php
@@ -2,14 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\NflOdds;
-use App\Models\NflScore;
 use App\Models\NflTeam;
+use App\Models\NflTeamSchedule;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Services\Elo\EloRatingSystem;
-
-// Adjust the namespace as needed
 
 class NflController extends Controller
 {
@@ -21,7 +18,7 @@ class NflController extends Controller
     {
         $this->apiKey = config('services.oddsapi.key');
         $this->baseUrl = config('services.oddsapi.base_url');
-        $this->eloRatingSystem = $eloRatingSystem; // Inject EloRatingSystem
+        $this->eloRatingSystem = $eloRatingSystem;
     }
 
     public function index()
@@ -29,17 +26,21 @@ class NflController extends Controller
         $teams = NflTeam::all();
         $expectedWins = $this->eloRatingSystem->calculateExpectedWinsForTeams();
 
-        return view('nfl.teams', compact('teams', 'expectedWins'));
-    }
+        // Fetch next three upcoming games for each team
+        $nextOpponents = [];
+        foreach ($teams as $team) {
+            $nextOpponents[$team->id] = NflTeamSchedule::where(function ($query) use ($team) {
+                $query->where('team_id_home', $team->id)
+                    ->orWhere('team_id_away', $team->id);
+            })
+                ->where('game_date', '>', '2024-03-01')
+                ->whereNull('home_result')
+                ->whereNull('away_result')
+                ->orderBy('game_date')
+                ->take(3)
+                ->get(['game_date', 'home', 'away', 'team_id_home', 'team_id_away']);
+        }
 
-    public function event(Request $request)
-    {
-        $selectedDate = $request->input('selectedDate', Carbon::today()->format('Y-m-d'));
-        $scores = NflScore::with('homeTeam', 'awayTeam')
-            ->whereDate('commence_time', $selectedDate)
-            ->get();
-        $odds = NflOdds::whereIn('event_id', $scores->pluck('event_id'))->get();
-
-        return view('nfl.event', compact('scores', 'odds', 'selectedDate'));
+        return view('nfl.teams', compact('teams', 'expectedWins', 'nextOpponents'));
     }
 }
