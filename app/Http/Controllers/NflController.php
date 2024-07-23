ff0431b55466de5cb0abf1bf0cdc6f94d64b4293
Bey <Josh@picksports.app>
2024-07-23 09:26:59 -0500

    errors text

--- app/Http/Controllers/NflController.php
+++ app/Http/Controllers/NflController.php
@@ -4,31 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Models\NflTeam;
 use App\Services\NflPredictionService;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class NflController extends Controller
 {
-    protected NflPredictionService $nflService;
+    protected NflPredictionService $nflPredictionService;
 
-    public function __construct(NflPredictionService $nflService)
+    public function __construct(NflPredictionService $nflPredictionService)
     {
-        $this->nflService = $nflService;
+        $this->nflPredictionService = $nflPredictionService;
     }
 
     public function index()
     {
-        $data = $this->nflService->getTeamsWithSchedulesAndOdds();
-
-        dd($data['expectedWins']); // Add this line to debug
-        return view('nfl.teams', $data);
-
+        try {
+            $data = $this->nflPredictionService->getTeamsWithSchedulesAndOdds();
+            Log::info('Expected Wins Data:', $data['expectedWins']);
+            return view('nfl.teams', $data);
+        } catch (Exception $e) {
+            Log::error('Error fetching teams with schedules and odds: ' . $e->getMessage(), [
+                'exception' => $e,
+            ]);
+            return response()->view('errors.500', [], 500);
+        }
     }
-
 
     public function show($teamId)
     {
-        $team = NflTeam::findOrFail($teamId);
-        $expectedWins = $this->nflService->eloRatingSystem->calculateExpectedWins($team->id);
-        return view('nfl.show', compact('team', 'expectedWins'));
+        try {
+            $team = NflTeam::findOrFail($teamId);
+            $expectedWins = $this->nflPredictionService->eloRatingSystem->calculateExpectedWins($team->id);
+            return view('nfl.show', compact('team', 'expectedWins'));
+        } catch (Exception $e) {
+            Log::error('Error fetching team details: ' . $e->getMessage(), [
+                'exception' => $e,
+            ]);
+            return response()->view('errors.500', [], 500);
+        }
     }
 }
