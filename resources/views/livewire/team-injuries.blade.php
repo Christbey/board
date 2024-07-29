b27bcca65fef9a407e79f861fc8fb69da651a5ee
Bey <Josh@picksports.app>
2024-07-29 18:03:07 -0500

    details and injuries view

--- resources/views/livewire/team-injuries.blade.php
+++ resources/views/livewire/team-injuries.blade.php
@@ -4,9 +4,13 @@
             <x-list-item>
                 <p><strong>Status:</strong> {{ $injury['status'] ?? 'N/A' }}</p>
                 <p><strong>Short Comment:</strong> {{ $injury['shortComment'] ?? 'N/A' }}</p>
-                <p><strong>Long Comment:</strong> {{ $injury['longComment'] ?? 'N/A' }}</p>
+                {{--
+                                <p><strong>Long Comment:</strong> {{ $injury['longComment'] ?? 'N/A' }}</p>
+                --}}
                 <p><strong>Date:</strong> {{ $injury['date'] ?? 'N/A' }}</p>
-                <p><strong>Athlete ID:</strong> {{ $injury['athlete']['id'] ?? 'N/A' }}</p>
+                {{--
+                                <p><strong>Athlete ID:</strong> {{ $injury['athlete']['id'] ?? 'N/A' }}</p>
+                --}}
             </x-list-item>
         @endforeach
     @else
